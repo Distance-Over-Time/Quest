ce4962c7b525e1d4d00a77fbd3fae79e4698e3f8
Ryan Davis <ryan.alex.davis@gmail.com>
2025-03-01 11:39:25 -0800

    Player Movement on Slope Fixed
    - Player can now move up and down slopes using Raycast hits

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     private Vector2 move;
+    public LayerMask terrainLayer;
+    public float groundDist;
 
     public void OnMove(InputAction.CallbackContext context) {
         move = context.ReadValue<Vector2>();
@@ -17,7 +19,20 @@ public class PlayerController : MonoBehaviour
         MovePlayer();
     }
 
-    public void MovePlayer() {
+    public void MovePlayer()
+    {        
+        // Snap player to ground
+        RaycastHit hit;
+        Vector3 castPos = transform.position;
+        castPos.y += 1;
+        if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer)) {
+            if (hit.collider != null) {
+                Vector3 movePos = transform.position;
+                movePos.y = hit.point.y + groundDist;
+                transform.position = movePos;
+            }
+        }
+
         Vector3 movement = new Vector3(move.x, 0f, move.y);
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }
