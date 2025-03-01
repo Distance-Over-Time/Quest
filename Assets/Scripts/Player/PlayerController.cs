4ef99e0274cc3d032b81d18606c2fe1d7dc5b7b8
Ryan Davis <ryan.alex.davis@gmail.com>
2025-03-01 10:00:19 -0800

    Committing Shader WIPs from Before Trip
    Portal, Base, and some textures

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     private Vector2 move;
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     public void OnMove(InputAction.CallbackContext context) {
         move = context.ReadValue<Vector2>();
@@ -20,5 +21,20 @@ public class PlayerController : MonoBehaviour
     public void MovePlayer() {
         Vector3 movement = new Vector3(move.x, 0f, move.y);
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        FlipSprite();
+    }
+
+    // Currently snaps back to facing left
+    public void FlipSprite() {
+        if (move.x > 0) {
+            spriteRenderer.flipX = true;
+        }
+        else {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    public void FaceNPC() {
+        // TODO: have player sprite face NPC when talking to one
     }
 }
