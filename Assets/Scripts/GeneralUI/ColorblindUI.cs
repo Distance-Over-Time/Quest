c8d84c36ba4d113c34e5417477b071359faed482
Ryan Davis <ryan.alex.davis@gmail.com>
2025-03-05 08:45:11 -0800

    Colorblind Plugin Implemented
    Wrote UI wrapper class for it so it's not on a cycle

--- Assets/Scripts/GeneralUI/ColorblindUI.cs
+++ Assets/Scripts/GeneralUI/ColorblindUI.cs
@@ -3,21 +3,35 @@ using UnityEngine.EventSystems;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using System.Reflection;
+using SOHNE.Accessibility.Colorblindness;
 
 public class ColorblindUI : MenuActivation
 {
+    private Colorblindness colorblindnessInstance;
+
     void Start() {
+        colorblindnessInstance = Colorblindness.Instance;
+
+        if (colorblindnessInstance == null) {
+            Debug.LogError("Colorblindness instance not found");
+        }
+
+        // Ensure we start on default mode/no colorblind filter
+        colorblindnessInstance.Change(0);
+
         gameObject.SetActive(false);
     }
 
-    void OnEnable()
-    {
+    void OnEnable() {
         EventSystem.current.SetSelectedGameObject(selectFirst);
     }
 
-    public void ChangeMode(int mode)
-    {
-        // TODO: change for SOHNE colorblindness
-        Debug.Log(mode);
+    public void SetColorblindMode(int modeIndex) {
+        if (colorblindnessInstance != null) {
+            colorblindnessInstance.Change(modeIndex);
+        }
+        else {
+            Debug.LogError("Colorblindness instance is null.");
+        }
     }
 }
