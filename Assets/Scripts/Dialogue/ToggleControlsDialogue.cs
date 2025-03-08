35763e5fcbac02798490a47f52ca26c98efcb665
Ryan Davis <ryan.alex.davis@gmail.com>
2025-03-08 09:06:19 -0800

    Update ToggleControlsDialogue.cs

--- Assets/Scripts/Dialogue/ToggleControlsDialogue.cs
+++ Assets/Scripts/Dialogue/ToggleControlsDialogue.cs
@@ -8,14 +8,17 @@ public class ToggleControlsDialogue : MonoBehaviour
 {
     [SerializeField] private PlayerInput playerControls;
     [SerializeField] private PlayerInput inventoryControls;
+    [SerializeField] private PlayerInput uiControls;
 
     public void DisableControls() {
         playerControls.DeactivateInput();
         inventoryControls.DeactivateInput();
+        uiControls.DeactivateInput();
     }
 
     public void EnableControls() {
         playerControls.ActivateInput();
         inventoryControls.ActivateInput();
+        uiControls.DeactivateInput();
     }
 }
